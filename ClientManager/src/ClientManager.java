3dc29e9f14622526daeaf18a5f17b2d42fb02833
SeimuPVE <paul.vanelsue@gmail.com>
2018-10-22 10:10:16 +0200

    Preparing ClientManager.

---
+++ ClientManager/src/ClientManager.java
@@ -0,0 +1,57 @@
+public class ClientManager {
+    public static void main(String[] argv) {
+        // Open the file.
+
+        // Load users.
+
+        // If there is not any admin, add it and load again.
+
+        // Print users.
+
+        // In a while.
+        // Ask for action (0=EXIT, 1=ADD, 2=CHANGE_PASSWORD, 3=DELETE)
+
+
+        System.out.println("Hello World !");
+    }
+
+    public static void adminAdder() {
+        // Ask for the user name.
+
+        // Ask for the password.
+
+        // Check the password.
+
+        // Add the admin.
+
+    }
+
+    public static void userAdder() {
+        // Ask for a new user.
+
+        // Check information.
+
+        // Add user.
+
+    }
+
+    public static void passwordChanger() {
+        // Ask for the user.
+
+        // Ask the current password.
+
+        // Check the current password.
+
+        // Change the password.
+
+    }
+
+    public static void deleteUser() {
+        // Ask for the user.
+
+        // Ask the current password (admin or user).
+
+        // Delete the user.
+
+    }
+}
