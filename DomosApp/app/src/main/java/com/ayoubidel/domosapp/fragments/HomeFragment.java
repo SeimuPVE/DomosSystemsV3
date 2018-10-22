426412139bb7f089a23420ffb54214ca9d6245ee
DevPsksh <ayoubidel64@gmail.com>
2018-10-22 14:11:23 +0200

    Update fragments

--- DomosApp/app/src/main/java/com/ayoubidel/domosapp/fragments/HomeFragment.java
+++ DomosApp/app/src/main/java/com/ayoubidel/domosapp/fragments/HomeFragment.java
@@ -1,8 +1,11 @@
 package com.ayoubidel.domosapp.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +14,29 @@ import com.ayoubidel.domosapp.R;
 public class HomeFragment extends Fragment {
 
     private View myView;
+    private  FloatingActionButton fab;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.home_fragment, container, false);
+
+
+        fab = (FloatingActionButton) myView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+
+
+
         return myView;
     }
 }
