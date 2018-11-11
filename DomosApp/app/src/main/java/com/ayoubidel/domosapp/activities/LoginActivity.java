a46fca56a7bbefcb0453e7917ea35176bff60123
Ayoub IDEL <ayoubidel64@gmail.com>
2018-11-11 08:54:21 +0100

    Fix return bug

--- DomosApp/app/src/main/java/com/ayoubidel/domosapp/activities/LoginActivity.java
+++ DomosApp/app/src/main/java/com/ayoubidel/domosapp/activities/LoginActivity.java
@@ -33,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
