5739078c70e06ca01355b136cdc4f037528c3276
Ayoub IDEL <ayoubidel64@gmail.com>
2018-11-11 08:52:06 +0100

    Create DataBaseHelper

---
+++ DomosApp/app/src/main/java/com/ayoubidel/domosapp/utils/DataBaseHelper.java
@@ -0,0 +1,23 @@
+package com.ayoubidel.domosapp.utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.ayoubidel.domosapp.adapters.ModuleDatabaseAdapter;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(ModuleDatabaseAdapter.DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
